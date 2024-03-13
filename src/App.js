e46843cf0e7c4561ee1d11dfc6ad08f8fadb88b7
Student <ann.ten.tkdlove@mail.ru>
2024-03-13 18:49:57 +0300

    sigma

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import logo from './Group 1.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={logo} className="App-header__img" alt="logo" />
+        <p className='App-header__text' >
+        Вход
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form className='App-header__form'>
+        <textarea className='App-header__textArea' placeholder='Логин'>
+        </textarea >
+        <textarea className='App-header__textArea' placeholder='Пароль'>
+        </textarea>
+        <button  type='submit' className='App-header__button'>
+          Войти
+        </button>
+        </form>
+        <div className='App-header__noAcc'>
+          <p className='App-header__noAcc-p'>Нет аккаунта?</p>
+          <a href='#' className='App-header__noAcc-link'><p className='App-header__noAcc-p'>Зарегистрироваться</p></a>
+        </div>
       </header>
     </div>
   );
